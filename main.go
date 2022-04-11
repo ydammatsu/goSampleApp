aafc38acb121070b6e48fc1d7453bf0411c5c648
dammatsu.yuta <dammatsu.yuta@moneyforward.co.jp>
2022-04-11 15:21:52 +0900

    db 接続処理を追加

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 )
 
 func main() {
+	db := sqlConnect()
+	defer db.Close()
+
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
 
@@ -13,4 +20,25 @@ func main() {
 	})
 
 	router.Run()
+}
+
+func sqlConnect() (database *gorm.DB) {
+	DBMS := "mysql"
+	USER := "go_test"
+	PASS := "password"
+	PROTOCOL := "tcp(db:3306)"
+	DBNAME := "go_database"
+
+	// CONNECT => USER:PASS@tcp(db:3306)/DBNAME?OPTIONS
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+
+	db, err := gorm.Open(DBMS, CONNECT)
+	if err != nil {
+		fmt.Println("DB接続失敗")
+		panic(err)
+	}
+
+	fmt.Println("DB接続成功")
+
+	return db
 }
